f905ba904e33563a45f2283ab82eec260895e698
Daniel Song <daniel.song@teradata.com>
2018-09-16 15:02:12 -0700

    Copy in examples

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -12,18 +12,18 @@ class SnippetSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         """
-        Create and return a new 'Snippet' instance with the validated data
+        Create and return a new `Snippet` instance, given the validated data.
         """
         return Snippet.objects.create(**validated_data)
 
-    def update(self, instance, validiated_data):
+    def update(self, instance, validated_data):
         """
-        Update and return an existing 'Snippet' instance with the validated data
+        Update and return an existing `Snippet` instance, given the validated data.
         """
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
         instance.linenos = validated_data.get('linenos', instance.linenos)
         instance.language = validated_data.get('language', instance.language)
-        instance.style = validiated_data.get('style', instance.style)
+        instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
